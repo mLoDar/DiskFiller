048b4edc21722d839f1396e39efc1f6d67150ef4
mLoDar <tomas.wampenwachser@gmail.com>
2023-06-09 22:09:26 +0200

    Renamed files

---
+++ DiskFiller.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+
+namespace DiskFiller
+{
+    public partial class DiskFiller : Form
+    {
+        public DiskFiller()
+        {
+            InitializeComponent();
+        }
+    }
+}
