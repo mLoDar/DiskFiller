a567f75331b092f986cf8307071f5b7923462ae4
mLoDar <mlodarcoding@gmail.com>
2024-03-22 21:51:32 +0100

    Added raw application process

--- DiskFiller.cs
+++ DiskFiller.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 
 
@@ -9,6 +8,30 @@ namespace DiskFiller
     {
         public DiskFiller()
         {
+            // Check if there is at least 4gb available
+                // Destroy self if space <= 4gb
+
+
+
+            // Define all needed variables
+
+
+
+            // Check if file is not in wanted folder
+                // Copy application to appdata
+                // Start application from new folder
+                // Exit and destroy current application
+
+
+
+            // Prepare the content which will be written to the files
+
+
+
+            // Start the writing
+
+
+
             InitializeComponent();
         }
     }
