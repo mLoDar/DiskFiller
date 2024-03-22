93188f39afe67bc5ce10a9f87187922c0baef8bc
mLoDar <mlodarcoding@gmail.com>
2024-03-22 21:51:32 +0100

    Renamed files

--- DiskFiller.Designer.cs
+++ DiskFiller.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DiskFiller
 {
-    partial class Form1
+    partial class DiskFiller
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // DiskFiller
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "DiskFiller";
+            this.Text = "DiskFiller";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
